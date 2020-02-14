10f7d1aeddd7c80aacdf793b6d36a6ba3325a720
Ben Orozco <benoror@gmail.com>
2020-02-13 20:10:32 -0700

    Initial BoilerPlate

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace metasource_feb2020
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            
+            Application.Run();
+        }
+    }
+}
